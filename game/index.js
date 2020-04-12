bbbed84cc1ea3d8c9fcbf6e2eaba6b7e9efdb3e0
A3hinav <62795494+A3hinav@users.noreply.github.com>
2020-04-12 23:34:44 +0530

    Update index.js

--- game/index.js
+++ game/index.js
@@ -10,27 +10,29 @@ var score1 = score2 = 0;
 
 if(randomNumber1 > randomNumber2){
     document.querySelector("h2").innerHTML = "🚩player 1 wins";
+    document.querySelector("h4").innerHTML = "player 1 is the winner with score = " +score1;
     score1 ++;
 }
 else if( randomNumber1 === randomNumber2)
 {
   document.querySelector("h1").innerHTML = "🚩Draw!🚩";
+    document.querySelector("h4").innerHTML = "It's a draw!";
   score1 = score2 = +1;
 }
 else
 {
    document.querySelector("h2").innerHTML = "player 2 wins🚩";
+     document.querySelector("h4").innerHTML = "player 2 is the winner with score = " +score2;
    score2 ++;
 }
 
 document.querySelector("h3").innerHTML = "\nplayer 1 Score = " +score1+ " ∆ player 2 Score = " +score2;
 
 if(score1 > score2){
- document.querySelector("h4").innerHTML = "player 1 is the winner with score = " +score1;
-} else if(score1 === score2){
-document.querySelector("h4").innerHTML = "It's a draw!";
+ } else if(score1 === score2){
+
 }
 else
 {
-  document.querySelector("h4").innerHTML = "player 2 is the winner with score = " +score2;
+ 
 }
